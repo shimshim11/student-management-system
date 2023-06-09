a3e8dea1828e63680c824832395af7ff2cba5bfb
Shimon <shimonkocor@gmail.com>
2023-06-09 15:06:42 +0100

    Added an about tab in the toolbar

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+        about_action.triggered.connect(self.about)
 
         search_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu_item.addAction(search_action)
@@ -90,6 +91,21 @@ class MainWindow(QMainWindow):
     def delete(self):
         dialog = DeleteDialog()
         dialog.exec()
+
+    def about(self):
+        dialog = AboutDialog()
+        dialog.exec()
+
+
+class AboutDialog(QMessageBox):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("About")
+        content = """
+        This is an app which uses a GUI to add, update and delete students
+        from a student management system.
+        """
+        self.setText(content)
 
 
 class EditDialog(QDialog):
